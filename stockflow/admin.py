5f4d0bbe70d9e9e0f63005dc73cec71a57f5098d
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-10-08 11:24:32 -0500

    Update profile photo function

--- stockflow/admin.py
+++ stockflow/admin.py
@@ -39,26 +39,44 @@ def get_user(id):
     user = User.query.get_or_404(id) # Query to user table
     return user
 
+
+from werkzeug.utils import secure_filename
+
+def get_profile_photo(id):
+    user = User.query.get_or_404(id)
+    profile_proto = ""
+    if profile_proto != "":
+        profile_proto = user.profile_photo
+
+    return profile_proto
+
 # Update user - profile
 @bp.route("/profile/edit/<int:id>", methods = ("GET", "POST"))
 @login_required
 def update_user(id):
     # Search on db user id
     user = get_user(id)
+    profile_photo = get_profile_photo(id)
 
     if request.method == "POST":
         user.names = request.form["names"]
         user.last_names = request.form["last_names"]
         user.username = request.form["username"]
         user.email = request.form["email"]
-        user.profile_photo = request.form["profile_photo"]
+
+        if request.files["profile_photo"]:
+            profile_photo = request.files["profile_photo"]
+            
+            # Save photo on a static project folder
+            profile_photo.save(f"stockflow/static/media/user/{secure_filename(profile_photo.filename)}") # Get file name
+            user.profile_photo = f"media/user/{secure_filename(profile_photo.filename)}"; # Save on db
 
         db.session.commit()
 
         # Redirection
         return redirect(url_for("admin.profile"))
 
-    return render_template("modules/users/profile/edit.html", user = user)
+    return render_template("modules/users/profile/edit.html", user = user, profile_photo = profile_photo)
 
 # Products
 @bp.route("/products/")

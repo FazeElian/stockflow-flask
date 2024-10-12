17befe67ed487e4ea56512e605805566639b3778
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-10-11 23:42:16 -0500

    Creation of Register sale function

--- stockflow/modules/sale.py
+++ stockflow/modules/sale.py
@@ -1,11 +1,21 @@
 from flask import (
     Blueprint, 
-    render_template
+    render_template,
+    request,
+    url_for,
+    redirect,
+    g
 )
 
 from stockflow.auth import login_required
 
 bp = Blueprint("admin/sales", __name__, url_prefix="/admin/sales")
+
+# Database
+from stockflow import db
+
+# Models
+from stockflow.models import Sale, Product, SaleItem, Customer
 
 # Sales
 @bp.route("/")
@@ -14,7 +24,59 @@ def index():
     return render_template("modules/sales/index.html")
 
 # New sale
-@bp.route("/new/")
+@bp.route("/new/", methods=("GET", "POST"))
 @login_required
 def new():
-    return render_template("modules/sales/new.html")
+    # Obtener todos los productos
+    products = Product.query.filter(Product.created_by == g.user.id).all()
+
+    # Obtener todos los clientes
+    customers = Customer.query.filter(Product.created_by == g.user.id).all()
+
+    if request.method == "POST":
+        # Create the new sale without products
+        sale = Sale(
+            created_by = g.user.id,
+            customer_id = request.form["customer"],
+            total = 0
+        )
+    
+        # Get the products selected and quantities
+        selected_products = request.form.getlist("products")
+        quantities = request.form.getlist("quantity")
+
+        # Add the sale
+        db.session.add(sale)
+        db.session.commit()
+
+        # Calculate the total sale
+        total_sale = 0
+
+        # Add the products list to sale using the id
+        for index, product_id in enumerate(selected_products):
+            product = Product.query.get(product_id)
+            quantity = int(quantities[index])
+
+            # Create a new sale item
+            sale_item = SaleItem(
+                created_by=g.user.id,
+                sale_id=sale.id, 
+                product_id=product.id, 
+                quantity=quantity, 
+                price=product.price
+            )
+
+            # Product total
+            total_sale += product.price * quantity
+
+            # Add the sale item
+            db.session.add(sale_item)
+
+        # Update the total sale value
+        sale.total = total_sale
+        db.session.commit() # Save all
+
+        # Redirection
+        return redirect(url_for("admin/sales.index"))
+
+    return render_template("modules/sales/new.html", products = products, customers = customers)
